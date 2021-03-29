401d62da60f57c01634617b80b473d12ad23d543
ThePrograminator <christian_skyder@hotmail.com>
2021-03-29 16:47:48 +0200

    first

---
+++ public/electron.js
@@ -0,0 +1,112 @@
+const electron = require("electron");
+//const app = electron.app;
+//const BrowserWindow = electron.BrowserWindow;
+//const Dialog = electron.Dialog
+const { app, BrowserWindow, dialog, Menu } = require("electron");
+const { autoUpdater } = require("electron-updater");
+
+const fs = require("fs");
+
+const path = require("path");
+const url = require("url");
+const isDev = require("electron-is-dev");
+
+const settings = require("electron-settings");
+
+//const isOnline = require("is-online");
+
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
+
+autoUpdater.on("checking-for-update", () => {
+  //console.log('Checking for updates...')
+  //sendStatusToWindow("Checking for update...");
+});
+
+autoUpdater.on("update-available", info => {
+  console.log("Update Available");
+  console.log("Version", info.version);
+  console.log("Release Date", info.releaseDate);
+  //sendStatusToWindow("Update available.");
+  mainWindow.webContents.send("update-available");
+});
+
+autoUpdater.on("update-not-available", () => {
+  console.log("Update not Available");
+  //sendStatusToWindow("Update not available.");
+  mainWindow.webContents.send("update-not-available");
+});
+
+autoUpdater.on("download-progress", progress => {
+  console.log(`Progress ${Math.floor(progress.percent)}`);
+  var data = Math.floor(progress.percent);
+  mainWindow.webContents.send("download-progress", data);
+});
+
+autoUpdater.on("update-downloaded", info => {
+  console.log("Update Downloaded");
+  //sendStatusToWindow("Update downloaded");
+  autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on("error", error => {
+  console.log(error);
+  //sendStatusToWindow("Error in auto-updater. " + error);
+});
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    icon: __dirname + "/icon/logo64x64.png",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  /*(async () => {
+    var onlineStatus = await isOnline();
+    console.log("onlineStatus", onlineStatus);
+    if (!isDev && onlineStatus) {
+      autoUpdater.checkForUpdates();
+    }
+  })();*/
+
+  /*if (!settings.has("databasePath")) {
+    settings.set("databasePath", "");
+    settings.set("folderPath", "");
+    settings.set("fileNameDelimiter", "-");
+    settings.set("fileNamePrepend", "ID");
+    settings.set("loadOnStart", false);
+    settings.set("quessColumnFilters", false);
+  }*/
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.setMenuBarVisibility(false)
+
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+	return (
+		<div className="App">
+			<p>Hello</p>
+		</div>
+	)
+}
+
+export default App
